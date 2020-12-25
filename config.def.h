c9596925d96227b5fe2aac280ad785a2865cd87d
tramhao <haoxinkui@gmail.com>
2020-12-25 11:09:56 +0800

    change a value for more transparency

--- config.def.h
+++ config.def.h
@@ -22,7 +22,7 @@ static char normfgcolor[]           = "#bbbbbb";
 static char selfgcolor[]            = "#eeeeee";
 static char selbordercolor[]        = "#770000";
 static char selbgcolor[]            = "#005577";
-static const unsigned int baralpha = 0xd0;
+static const unsigned int baralpha = 0xc5;
 static const unsigned int borderalpha = OPAQUE;
 static char *colors[][3] = {
        /*               fg           bg           border   */
